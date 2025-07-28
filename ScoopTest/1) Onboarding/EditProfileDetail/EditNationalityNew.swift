76461eac57bc93cea12c09996b2189817e3f2c61
Arthur-Ostin <164878793+Arthur-Ostin@users.noreply.github.com>
2025-07-28 11:54:29 -0400

    DO NOT USE

---
+++ ScoopTest/1) Onboarding/EditProfileDetail/EditNationalityNew.swift
@@ -0,0 +1,18 @@
+//
+//  EditNationalityNew.swift
+//  ScoopTest
+//
+//  Created by Art Ostin on 28/07/2025.
+//
+
+import SwiftUI
+
+struct EditNationalityNew: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditNationalityNew()
+}
