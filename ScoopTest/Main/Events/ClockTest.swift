bdf623dd252eca171ffa43946ba1da9db9a6dd27
Arthur-Ostin <164878793+Arthur-Ostin@users.noreply.github.com>
2025-08-05 11:23:12 -0400

    DO NOT USE

--- ScoopTest/Main/Events/ClockTest.swift
+++ ScoopTest/Main/Events/ClockTest.swift
@@ -6,37 +6,46 @@
 //
 
 import SwiftUI
+import Combine
 
-struct ClockTest: View {
-    @State private var countdown = CountdownViewModel(dateKey: "dailyProfilesDate")
+
+@Observable class countdownViewModel {
+    
+    
+    
+    
+    
+    
+}
+
+
+struct countdownTimer: View {
+    
+    
+    let meetUpTime: Date?
+    var cancellables = Set<AnyCancellable>()
+    
+    var hourRemaining = ""
+    var minuteRemaining = ""
+    var secondRemaining = ""
+    
     
     var body: some View {
-        countdownTimer
-    }
-}
-
-#Preview {
-    ClockTest()
-}
-
-
-extension ClockTest {
-    
-    
-    private var countdownTimer: some View {
+        
         HStack(spacing: 32) {
-            clockSection(time: countdown.hourRemaining, sign: "hr")
-            clockSection(time: countdown.minuteRemaining, sign: "m")
-            clockSection(time: countdown.secondRemaining, sign: "s")
+            clockSection(time: hourRemaining, sign: "hr")
+            clockSection(time: minuteRemaining, sign: "m")
+            clockSection(time: secondRemaining, sign: "s")
         }
         .foregroundStyle(.white)
         .frame(width: 253, height: 52)
         .background(Color.accent)
         .cornerRadius(15)
         .shadow(color: .black.opacity(0.15), radius: 1, x: 0, y: 2)
+        
+        
     }
-
-    private func clockSection(time: String, sign: String) -> some View {
+    func clockSection(time: String, sign: String) -> some View {
         HStack(spacing: 5) {
             Text(time)
                 .font(.custom("SFCompactRounded-Semibold", size: 28))
@@ -44,5 +53,24 @@ extension ClockTest {
                 .font(.custom("SFCompactRounded-Regular", size: 14))
                 .offset(y: 5)
         }
+    }
+    
+    mutating func starTimer() {
+        Timer
+            .publish(every: 1.0, on: .main, in: .common).autoconnect()
+            .sink {_ in updateTimeRemaining()}
+            .store(in: &cancellables)
+    }
+    
+    mutating func updateTimeRemaining() {
+        guard let meetUpTime = meetUpTime else { return }
+        
+        let timeRemaining = Calendar.current.dateComponents([.hour, .minute, .second], from: Date(), to: meetUpTime)
+        let hour = max(0, timeRemaining.hour ?? 0)
+        let minute = max(0, timeRemaining.minute ?? 0)
+        let second = max(0, timeRemaining.second ?? 0)
+        hourRemaining = String(format: "%02d", hour)
+        minuteRemaining = String(format: "%02d", minute)
+        secondRemaining = String(format: "%02d", second)
     }
 }
