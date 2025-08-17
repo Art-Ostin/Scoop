2af4369bcde63a72723337eca1fbc6915b74e311
Arthur-Ostin <164878793+Arthur-Ostin@users.noreply.github.com>
2025-08-17 08:04:02 -0400

    DO NOT USE

--- ScoopTest/Main/Meet/MeetViewModel.swift
+++ ScoopTest/Main/Meet/MeetViewModel.swift
@@ -1,75 +1,18 @@
 //
-//  MeetUpViewModel.swift
+//  MeetViewModel.swift
 //  ScoopTest
 //
-//  Created by Art Ostin on 07/08/2025.
+//  Created by Art Ostin on 17/08/2025.
 //
 
-import Foundation
 import SwiftUI
 
+struct MeetViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
 
-@Observable class MeetViewModel {
-    
-    var dep: AppDependencies
-    var profileRecs: [EventInvite] = []
-    var profileInvites: [EventInvite] = []
-    
-    var time: Date?
-    
-    init(dep: AppDependencies) {
-        self.dep = dep
-        self.time = dep.defaultsManager.getSuggestedProfilesTimer()
-        Task {
-            if profileRecs.isEmpty { await loadProfileRecs() }
-            if profileInvites.isEmpty { await loadEventInvites() }
-        }
-    }
-    
-    func loadProfileRecs() async {
-        
-        let manager = dep.defaultsManager
-        let ids = manager.getSuggestedProfiles()
-        
-        guard time != nil else {
-            manager.removeAllSuggestedProfiles()
-            profileRecs = []
-            return
-        }
-        
-        let results = await withTaskGroup(of: UserProfile?.self, returning: [UserProfile].self) { group in
-            for id in ids {
-                group.addTask { try? await self.dep.profileManager.getProfile(userId: id) }
-            }
-            return await group.reduce(into: []) { result, element in if let element { result.append(element) } }
-        }
-        profileRecs = results
-        await dep.cacheManager.loadProfileImages(results)
-        
-        print("Function successfully called")
-    }
-    
-    func updateTwoDailyProfiles() async {
-        guard let newProfiles = try? await dep.profileManager.getRandomProfile() else { return }
-        await dep.cacheManager.loadProfileImages(newProfiles)
-        profileRecs = newProfiles
-        dep.defaultsManager.setSuggestedProfiles(newProfiles)
-    }
-    
-    func loadEventInvites() async {
-        
-        guard let events = try? await dep.eventManager.getUpcomingInvitedEvents(), !events.isEmpty else { return }
-        
-        let results = await withTaskGroup(of: EventInvite?.self, returning: [EventInvite].self) {group in
-            for e in events {
-                group.addTask {
-                    guard let p = try? await self.dep.profileManager.getProfile(userId: e.otherUserId) else {return nil }
-                        return EventInvite(p, e)
-                    }
-                }
-            return await group.reduce(into: []) {result, element in if let element { result.append(element)}}
-            }
-        profileInvites = results
-        await dep.cacheManager.loadProfileImages(results.map(\.profile))
-    }
+#Preview {
+    MeetViewModel()
 }
