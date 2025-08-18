c2a84582cbbddb3fd2903d56f3485fcf1f11a48a
Arthur-Ostin <164878793+Arthur-Ostin@users.noreply.github.com>
2025-08-18 10:23:02 -0400

    DO NOT USE

--- ScoopTest/Core/Services/UserManager.swift
+++ ScoopTest/Core/Services/UserManager.swift
@@ -18,41 +18,46 @@ class UserManager {
     private var userCollection: CollectionReference { Firestore.firestore().collection("users") }
     private func userDocument(userId: String) -> DocumentReference { userCollection.document(userId)}
 
-    
     private(set) var user: UserProfile? = nil
-    
-    
     
     func createUser (authUser: AuthDataResult) async throws {
         let uid = authUser.user.uid
         let profileUser = UserProfile(auth: authUser)
         try userDocument(userId: uid).setData(from: profileUser)
     }
-    func loadUser() async throws {
+    func loadCurrentUser() async throws {
         let uid = try auth.fetchAuthUser()
-        let profile = try await fetchProfile(userId: uid)
+        let profile = try await fetchUser(userId: uid)
         self.user = profile
     }
-    func updateUser(values: [UserProfile.CodingKeys : Any]) async throws {
+    func updateCurrentUser(values: [UserProfile.CodingKeys : Any]) async throws {
         let uid = try auth.fetchAuthUser()
         var data: [String: Any] = [:]
         for (key, value) in values { data[key.rawValue] = value }
         try await userDocument(userId: uid).updateData(data)
-        try? await loadUser()
+        try? await loadCurrentUser()
     }
-    func updateUserPrompt(index: Int, prompt: PromptResponse) async throws {
-        let key: UserProfile.CodingKeys
-        switch index {
-        case 1: key = .prompt1
-        case 2: key = .prompt2
-        case 3: key = .prompt3
-        default: return
-        }
-        let encoded = try Firestore.Encoder().encode(prompt)
-        try await updateUser(values: [key: encoded])
-    }
-        
-    func fetchProfile(userId: String) async throws -> UserProfile {
+    func fetchUser(userId: String) async throws -> UserProfile {
         try await userDocument(userId: userId).getDocument(as: UserProfile.self)
     }
 }
+
+
+
+
+// UpdateUser Prompt (Reshuffle so not a function just for this and can use it with updateCurrentUser
+/*
+ func updateUserPrompt(index: Int, prompt: PromptResponse) async throws {
+     let key: UserProfile.CodingKeys
+     switch index {
+     case 1: key = .prompt1
+     case 2: key = .prompt2
+     case 3: key = .prompt3
+     default: return
+     }
+     let encoded = try Firestore.Encoder().encode(prompt)
+     try await updateUser(values: [key: encoded])
+ }
+ 
+ */
+
