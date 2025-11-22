b65fe83f7c0884fac4d9127f2a627b363fc22822
Arthur-Ostin <164878793+Arthur-Ostin@users.noreply.github.com>
2025-11-21 19:00:50 -0500

    Updated Onboarding Process to more modern design

---
+++ Main/ProfileEditing/EditSex.swift
@@ -0,0 +1,18 @@
+//
+//  EditSex.swift
+//  Scoop
+//
+//  Created by Art Ostin on 21/11/2025.
+//
+
+import SwiftUI
+
+struct EditSex: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditSex()
+}
