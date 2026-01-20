70017abc5e71f25ad939154af1ce8e11fc10e4d7
Arthur-Ostin <164878793+Arthur-Ostin@users.noreply.github.com>
2026-01-20 08:07:48 -0500

    Added the loading screen and the draggable details View for the profile

---
+++ Core/Resources/Facts.swift
@@ -0,0 +1,18 @@
+//
+//  Facts.swift
+//  Scoop
+//
+//  Created by Art Ostin on 20/01/2026.
+//
+
+import SwiftUI
+
+struct Facts: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Facts()
+}
---
+++ Main/Profile/Profile/DetailsExtraInfo.swift
@@ -0,0 +1,18 @@
+//
+//  DetailsExtraInfo.swift
+//  Scoop
+//
+//  Created by Art Ostin on 20/01/2026.
+//
+
+import SwiftUI
+
+struct DetailsExtraInfo: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailsExtraInfo()
+}
