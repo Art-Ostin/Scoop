baf39bbb08255085e97bbc78ac915bb44f4eab22
Arthur-Ostin <164878793+Arthur-Ostin@users.noreply.github.com>
2026-01-30 17:13:45 -0500

    Updated the selectTime View

--- Main/Meet/Invite/SelectTimeView.swift
+++ Main/Meet/Invite/SelectTimeView.swift
@@ -13,105 +13,93 @@ struct SelectTimeView: View {
     
     @State private var hour: Int = 22
     @State private var minute: Int = 30
-    
     @State private var selectedDay: Int? = nil
+    let availableHours = [17, 18, 19, 20, 21, 22, 23, 00, 01, 02]
     
-    let columns: [GridItem] =  Array(repeating: GridItem(.flexible()), count: 7)
-
-    
-    let dayCount = 11 //Meet up, up to 10 days in future, not including today
+    private let columns: [GridItem] = Array(repeating: GridItem(.flexible()), count: 7)
+    private let dayCount = 11 //Meet up, up to 10 days in future, not including today
     
     var body: some View {
-        
         DropDownMenu(cornerRadius: 16) {
-            
-            dayPicker
-            
-            Divider()
-            
-            timePicker
+            VStack(spacing: 12) {
+                dayPicker
+                Divider()
+                timePicker
+            }
         }
-        .onAppear { syncWithEvent() }
-        
+        .onAppear { syncWithEvent() ; updateTime()}
+        .onChange(of: hour) {updateTime()}
+        .onChange(of: minute) {updateTime()}
+        .onChange(of: selectedDay) { updateTime()}
     }
 }
 
+//Views
 extension SelectTimeView {
     
-    private var dayPicker: some View {
-        
-        let nextElevenDays: [Date] = {
-            let calendar = Calendar.current
-            let today = Date()
-            return (0..<dayCount).compactMap { offset in
-                calendar.date(byAdding: .day, value: offset, to: today)
-            }
-        }()
-        
-        return LazyVGrid(columns: columns, spacing: 24) {
-            
-                //First lay out the days of the week at top then all the days
-                ForEach(0..<7) { idx in
-                    Text(nextElevenDays[idx], format: .dateTime.weekday(.narrow))
-                        .font(.body(12, .bold))
-                }
-                
-                ForEach(nextElevenDays.indices, id: \.self) { idx in
-                    event(day: nextElevenDays[idx], idx: idx)
-            }
+    private var days: [Date] {
+        let calendar = Calendar.current
+        let startOfToday = calendar.startOfDay(for: Date())
+        return (0..<dayCount).compactMap { offset in
+            calendar.date(byAdding: .day, value: offset, to: startOfToday)
         }
     }
     
-    @ViewBuilder
-    private func event(day: Date, idx: Int) -> some View {
-        let isToday = Calendar.current.isDateInToday(day)
-        
-        let isSelected = selectedDay == idx
-        
-        Text(day, format: .dateTime.day())
-            .font(.body(18))
-            .foregroundStyle(isToday ? Color.grayPlaceholder : (isSelected ? Color.accent : .black))
-            .background(
-                Circle()
-                    .stroke(100, lineWidth: 1, color: isSelected ? .accent : .clear)
-            )
-            .onTapGesture {
-                selectedDay = idx
-                var components = Calendar.current.dateComponents([.year, .month, .day], from: day)
-                components.hour = hour
-                components.minute = minute
-                vm.event.time = Calendar.current.date(from: components)
+    private var dayPicker: some View {
+        return LazyVGrid(columns: columns, spacing: 12) {
+            ForEach(0..<7) {idx in
+                Text(days[idx], format: .dateTime.weekday(.narrow))
+                    .font(.body(12, .bold))
             }
+            ForEach(days.indices, id: \.self) { idx in
+                event(idx: idx)
+            }
+        }
+    }
+
+    @ViewBuilder
+    private func event(idx: Int) -> some View {
+        let day = days[idx]
+        let isToday = Calendar.current.isDateInToday(day)
+        let isSelected = selectedDay == idx
+        Button {
+            if !isToday { selectedDay = idx }
+        } label : {
+            Text(day, format: .dateTime.day())
+                .font(.body(18, isSelected ? .bold : .medium))
+                .foregroundStyle(isToday ? Color.grayPlaceholder : .black)
+                .frame(width: 30, height: 30)
+                .background(
+                    Circle()
+                        .foregroundStyle(isSelected ? Color.accent.opacity(0.2) : Color.clear)
+                )
+                .contentShape(.rect)
+        }
     }
     
     private var timePicker: some View {
         HStack {
             Picker("Hour", selection: $hour) {
-                ForEach(0..<24, id: \.self) { h in
+                ForEach(availableHours, id: \.self) { h in
                     Text(String(format: "%02d", h)).tag(h)
                 }
             }
             
             Picker("Minute", selection: $minute) {
-                ForEach([0, 15, 30, 45], id: \.self) { m in
+                ForEach([00, 15, 30, 45], id: \.self) { m in
                     Text(String(format: "%02d", m)).tag(m)
                 }
             }
         }
         .pickerStyle(.wheel)
-        .frame(width: 160, height: 150)
+        .frame(width: 160, height: 130)
         .labelsHidden()
-        .onChange(of: hour) {updateTime()}
-        .onChange(of: minute) {updateTime()}
-        .onAppear {updateTime()}
+        .tint(Color.red)
     }
-    
-    private func updateTime() {
-        var components = Calendar.current.dateComponents([.year, .month, .day], from: vm.event.time ?? Date())
-        components.hour = hour
-        components.minute = minute
-        vm.event.time = Calendar.current.date(from: components)
-    }
+}
+
+//Functions
+extension SelectTimeView {
     
     private func syncWithEvent() {
         guard let date = vm.event.time else { return }
@@ -127,32 +115,16 @@ extension SelectTimeView {
             selectedDay = days
         }
     }
+    
+    private func updateTime() {
+        let calendar = Calendar.current
+        let baseDate: Date = {
+            guard let selectedDay else { return vm.event.time ?? Date()}
+            return days[selectedDay]
+        }()
+        var components = calendar.dateComponents([.year, .month, .day], from: baseDate)
+        components.hour = hour
+        components.minute = minute
+        vm.event.time = calendar.date(from: components)
+    }
 }
-
-
-    
-    
-/*
- ForEach(nextElevenDays.indices, id: \.self) { idx in
-     
-     let day = nextElevenDays[idx]
-     let today = Calendar.current.isDateInToday(day)
-     
-     
-     VStack(spacing: 24) {
-         Text(day, format: .dateTime.weekday(.narrow))
-             .font(.body(12, .bold))
-         
-         Group {
-             Text(day, format: .dateTime.day())
-             
-             Text(day, format: .dateTime.day())
-                 .padding(.top, 6)
-         }
-     }
- }
- */
-    
-    
-
-
