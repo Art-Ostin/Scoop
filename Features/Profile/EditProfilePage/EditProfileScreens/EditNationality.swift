2515540aaebdf61c2553b8341fb015ae97290cb1
Arthur-Ostin <164878793+Arthur-Ostin@users.noreply.github.com>
2026-02-02 07:53:53 -0500

    Finished organisation

--- Features/Profile/EditProfilePage/EditProfileScreens/EditNationality.swift
+++ Features/Profile/EditProfilePage/EditProfileScreens/EditNationality.swift
@@ -310,3 +310,11 @@ struct CustomScrollTab<Content: View>: View {
             .padding(.vertical, 12)
     }
 }
+
+
+extension Array where Element == String {
+    mutating func toggle(_ s: String, limit: Int? = nil) {
+        if let i = firstIndex(of: s) { remove(at: i) }
+        else if limit.map({ count < $0 }) ?? true { append(s) }
+    }
+}
