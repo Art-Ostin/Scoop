665a7758ca8c50cdeda274e73556c37326e15b31
Arthur-Ostin <164878793+Arthur-Ostin@users.noreply.github.com>
2026-03-13 10:20:59 -0400

    Fixing up and cleaning ProfileCard

--- Features/Meet/ProfileCard.swift
+++ Features/Meet/ProfileCard.swift
@@ -10,121 +10,40 @@ import SwiftUI
 struct ProfileCard : View {
     
     @Bindable var vm: MeetViewModel
-        
-    let pendingProfile: PendingProfile?
+    @Binding var selectedProfile: UserProfile?
     
-    let eventProfile: EventProfile?
-        
+    let userProfile: UserProfile
+    let image: UIImage
+    let event: UserEvent?
     let size: CGFloat
     
     let onTap: () -> ()
     
-    
-    
-    
-    
     var body: some View {
-        VStack {
-            if let image = profile.image {
-                Image(uiImage: image)
-                    .resizable()
-                    .defaultImage(size)
-                    .shadow(color: .black.opacity(0.25), radius: 2, x: 0, y: 4)
-                    .overlay(alignment: .bottomLeading) {
-                        HStack(alignment: .bottom) {
-                            VStack(alignment: .leading, spacing: 8) {
-                                HStack {
-                                    Text(profile.profile.name)
-                                        .font(.body(22, .bold))
-                                    
-                                                                        
-                                    
-                                }
-                                infoSection
-                                    .font(.body(14, .medium))
-                            }
-                            
-                            .foregroundStyle(.white)
-                            Spacer()
-                            inviteButton
-                        }
-                        .padding(.vertical, 16)
-                        .padding(.horizontal)
-                    }
-                    }
-            }
-        }
+        Image(uiImage: image)
+            .resizable()
+            .defaultImage(size)
+            .shadow(color: .black.opacity(0.25), radius: 2, x: 0, y: 4)
+            .overlay(alignment: .bottomLeading) { cardOverlay }
     }
+}
+
 
 extension ProfileCard {
-
-    @ViewBuilder
-    private var infoSection: some View {
-        if let event = profile.event {
-            let dates = event.proposedTimes.dates
-                .filter(\.stillAvailable)
-                .map(\.date)
-            
-            if !dates.isEmpty {
-                eventInfoView(dates: dates)
-                    .overlay(alignment: .topTrailing) {
-                        (
-                            Text(event.type.description.emoji ?? "")
-                                .font(.body(15, .medium))
-                            
-                            +
-                            Text(" \(event.type.description.label)")
-                                .font(.body(16, .medium))
-                        )
-                        .offset(y: -28)
-                    }
-            } else {
-                profileInfoView
-            }
-        } else {
-            profileInfoView
+    
+    private var cardOverlay: some View {
+        HStack(alignment: .bottom) {
+            infoSection
+            Spacer()
+            inviteButton
         }
+        .padding(.vertical, 16)
+        .padding(.horizontal)
     }
     
-    @ViewBuilder
-    private func eventInfoView(dates: [Date]) -> some View {
-        Group {
-            if dates.count == 1 {
-                Text(formatTime(date: dates.first))
-            } else if dates.count == 2 {
-                (
-                    Text(formatTime(date: dates.first, withHour: false, wideWeek: false))
-                    +
-                    Text(" | ")
-                    +
-                    Text(formatTime(date: dates.last, withHour: false, wideWeek: false))
-                    + Text ( " · ")
-                    +
-                    Text(formatTime(date: dates.first, onlyHour: true))
-                )
-            } else if dates.count == 3 {
-                HStack(spacing: 0) {
-                    ForEach(Array(dates.enumerated()), id: \.element) { index, date in
-                        let suffix = (index < dates.count - 1) ? ", " : " · "
-                        Text(formatTime(date: date, withHour: false, wideWeek: false) + suffix)
-                    }
-                    if let first = dates.first {
-                        Text(formatTime(date: first, onlyHour: true))
-                    }
-                }
-            }
-        }
-    }
-        
-    
-    private var profileInfoView: some View {
-        Text("\(profile.year) | \(profile.degree) | \(profile.hometown)")
-            .font(.body(14, .medium))
-    }
-        
     private var inviteButton: some View {
         Button {
-            quickInvite = profile
+            onTap()
         } label: {
             Image("LetterIconProfile")
                 .resizable()
@@ -135,41 +54,71 @@ extension ProfileCard {
         .frame(width: 40, height: 40)
         .background(
             Circle()
-                .fill(isInvite ? Color.appGreen : Color.accent)
+                .fill(event != nil ? Color.appGreen : Color.accent)
                 .shadow(color: .black.opacity(0.15), radius: 1, x: 0, y: 2)
         )
     }
+    
+    private var infoSection: some View {
+        VStack(alignment: .leading, spacing: 8) {
+            Text(userProfile.name)
+                .font(.body(22, .bold))
+            
+            if let event {
+                cardEventInfo(event: event)
+            } else {
+                cardProfileInfo
+            }
+        }
+        .foregroundStyle(Color.white)
+        .font(.body(14, .medium))
+    }
+    
+    private var cardProfileInfo: some View {
+        Text("\(userProfile.year) | \(userProfile.degree) | \(userProfile.hometown)")
+            .font(.body(14, .medium))
+    }
+    
+    @ViewBuilder
+    private func cardEventInfo(event: UserEvent) -> some View {
+        let dates = event.proposedTimes.dates.filter(\.stillAvailable).map(\.date)
+        Group {
+            if dates.count == 1 {
+                Text(formatTime(date: dates.first))
+            } else if dates.count == 2 {
+                twoDateView(dates: dates)
+            } else if dates.count == 3 {
+                threeDateView(dates: dates)
+            }
+        }
+        .overlay {eventInfoView(event: event)}
+    }
+    
+    private func eventInfoView(event: UserEvent) -> some View {
+        Text("\(event.type.description.emoji ?? "") \(event.type.description.label)")
+            .font(.body(16, .medium))
+            .offset(y: -28)
+    }
+
+    private func twoDateView(dates: [Date]) -> some View {
+        Text(
+            "\(formatTime(date: dates.first, withHour: false, wideWeek: false)) | " +
+            "\(formatTime(date: dates[1], withHour: false, wideWeek: false)) · " +
+            "\(formatTime(date: dates.first, onlyHour: true))"
+        )
+    }
+    
+    private func threeDateView(dates: [Date]) -> some View {
+        let dayText = dates
+            .map { formatTime(date: $0, withHour: false, wideWeek: false) }
+            .joined(separator: ", ")
+        
+        return Text("\(dayText) · \(formatTime(date: dates[0], onlyHour: true))")
+    }
 }
 
+//Logic dealing with the eventInfo
+extension ProfileCard {
+    
+}
 
-//Old Expiry time
-/*
- if let time = profile.event. {
-     HStack(spacing: 4) {
-         Text("Expires in:")
-         SimpleClockView(targetTime: time) {}
-     }
-     .font(.body(10, .regular))
-     .foregroundColor(Color(red: 0.58, green: 0.58, blue: 0.58))
-     .frame(maxWidth: .infinity, alignment: .trailing)
-     .padding(.horizontal, 24)
- }
- */
-
-/*
- 
-//            if let meet = profile.profile.idealMeetUp {
-//                let weekDay = meet.time.formatted(.dateTime.weekday(.wide))
-//                let hour = meet.time.formatted(.dateTime.hour(.twoDigits(amPM: .omitted)).minute())
-//                HStack {
-//                    Text(weekDay + " " + hour)
-//                    Text("|")
-//                        .foregroundStyle(Color.grayPlaceholder)
-//
-//                    Text(meet.type.description.label)
-//                }
-//                .font(.body(16, .regular))
-//
-//            } else {
-
- */
