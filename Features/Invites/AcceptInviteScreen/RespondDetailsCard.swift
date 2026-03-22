a8dd5da24f854481cc383c387a10c61fb96547d2
Arthur-Ostin <164878793+Arthur-Ostin@users.noreply.github.com>
2026-03-22 11:18:39 -0400

    Setting up the How it Works Screen.

--- Features/Invites/AcceptInviteScreen/RespondDetailsCard.swift
+++ Features/Invites/AcceptInviteScreen/RespondDetailsCard.swift
@@ -7,36 +7,59 @@
 
 import SwiftUI
 
+enum DetailInfo: CaseIterable {
+    
+    case time, message, event
+    
+    func title(_ event: UserEvent) -> String {
+        switch self {
+        case .time: return "Select A Time"
+        case .message: return "Message"
+        case .event: return event.type.title
+        }
+    }
+    
+    func message(_ event: UserEvent) -> String {
+        switch self {
+        case .time:
+            return "Select a time \(event.otherUserName) proposed or suggest a new time or event"
+        case .message:
+            return "Once accepted, message to sort out details and to find one another"
+        case .event:
+            return event.type.howItWorks(userEvent: event)
+        }
+    }
+    
+    var image: String {
+        switch self {
+        case .time: return "MiniClockIcon"
+        case .message: return "SmallMessageIcon"
+        case .event: return "FilledCup"
+        }
+    }
+}
 struct RespondDetailsCard: View {
     
     let event: UserEvent
     @Binding var isFlipped: Bool
-
-    let image: UIImage
     
-    var openingMessage: String {
-        event.proposedTimes.availableDates().count > 0
-        ? "Select a time \(event.otherUserName) proposed, or suggest a new time"
-        : "\(event.otherUserName)'s proposed times have expired. So, first, select a time you're availble"
-    }
+    let image: UIImage
     
     var body: some View {
         VStack(alignment: .leading, spacing: 24) {
-            
-            inviteDetailsTitle
-            
-            Text(openingMessage)
-                .font(.body(16, .italic))
-                .foregroundStyle(Color.grayText)
-            
-            Text("When you've accepted their invite you can begin messaging")
-                .font(.body(16, .bold))
-            
-            Text(event.type.howItWorks(userEvent: event))
-                .font(.body(16, .medium))
+            title
+            ForEach(DetailInfo.allCases, id: \.self) {detail in
+                DetailSection(event: event, type: detail)
+            }
         }
-        .lineSpacing(8)
-        .padding(22)
+        .lineSpacing(4)
+        .padding(.top, 18)
+        .padding(.horizontal, 22)
+        .padding(.bottom, 36)
+        .overlay(alignment: .bottomTrailing) {
+            inviteButton
+                .padding()
+        }
         .frame(maxWidth: .infinity)
         .background(CardBackground())
         .padding(.horizontal, 24)
@@ -45,93 +68,47 @@ struct RespondDetailsCard: View {
 
 extension RespondDetailsCard {
     
-    private var Title: some View {
+    private var title: some View {
         Text("How it Works")
-            .font(.body(12, .bold))
-            .tracking(1.2)
-            .foregroundStyle(Color.grayText)
+            .font(.custom("SFProRounded-Bold", size: 18))
+            .frame(maxWidth: .infinity, alignment: .center)
     }
     
-    @ViewBuilder
-    private var inviteDetailsTitle: some View {
-        HStack {
-            Text ("\(event.type.description.emoji) \(event.type.description.label)")
-                .font(.custom("SFProRounded-Medium", size: 20))
-            
-            Spacer()
-            backToEvent
-        }
-    }
-    
-    private var backToEvent: some View {
+    private var inviteButton: some View {
         Button {
             isFlipped.toggle()
         } label: {
-            HStack(spacing: 8) {
-                CirclePhoto(image: image, showShadow: false, height: 25)
-                Text("Event")
-                    .font(.custom("SFProRounded-Bold", size: 16))
+            HStack {
+                CirclePhoto(image: image, height: 24)
+                
+                Text("Invite")
+                    .font(.body(14, .bold))
                     .foregroundStyle(Color.appGreen)
-                    .contentShape(.rect)
             }
-            .background (
-                ZStack {
-                    RoundedRectangle(cornerRadius: 30)
-                        .fill(Color.background)
-                        .shadow(color: .appGreen.opacity(0.1), radius: 5, x: 0, y: 4)
-
-                    RoundedRectangle(cornerRadius: 30)
-                        .inset(by: 0.5)
-                        .stroke(Color.grayBackground, lineWidth: 0.5)
-                }
-            )
+            .padding(.horizontal, 8)
+            .padding(.vertical, 3)
+            .stroke(24, lineWidth: 1, color: .appGreen.opacity(0.2))
         }
     }
 }
 
-/*
- 
- private var inviteDetailsTitle: some View {
-     
-     HStack {
-         HStack(spacing: 8) {
-            Image(systemName: "info.circle")
-                 .font(.body(20, .regular))
-                  .foregroundStyle(Color.grayText)
-             
-             HStack(spacing: 2) {
-                 Text("How it works: ")
-                 
-                 +
-                 
-                 Text("\(event.type.description.label)")
-                     .font(.custom("SFProRounded-Bold", size: 20))
-                 
-                 Text("\(event.type.description.emoji)")
-                     .font(.body(20))
-             }
-         }
-         
-         Spacer()
-         
-         backToEvent
-     }
- }
 
- 
- 
- Text("\(event.type.description.emoji)  \(event.type.description.label)")
-     .font(.custom("SFProRounded-Bold", size: 24))
- 
- Spacer()
- 
- Button {
-     isFlipped = false
- } label: {
-     Text("Event")
-         .foregroundStyle(Color.appGreen)
-         .font(.body(16, .bold))
- }
+private struct DetailSection: View {
+    let event: UserEvent
+    let type: DetailInfo
+    
+    var body: some View {
+        HStack(spacing: 16) {
+            Image(type.image)
+            
+            VStack(alignment: .leading, spacing: 4) {
+                Text(type.title(event))
+                    .font(.body(16, .medium))
+                
+                Text(type.message(event))
+                    .font(.footnote)
+                    .foregroundStyle(.gray)
+            }
+        }
+    }
 }
-
- */
