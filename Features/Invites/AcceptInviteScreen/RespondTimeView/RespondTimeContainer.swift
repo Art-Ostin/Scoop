445daeb1fbbbe04cdd91871ed119f3bade709a1e
Arthur-Ostin <164878793+Arthur-Ostin@users.noreply.github.com>
2026-03-23 08:20:22 -0400

    Adding the customTime

---
+++ Features/Invites/AcceptInviteScreen/RespondTimeView/RespondTimeContainer.swift
@@ -0,0 +1,18 @@
+//
+//  RespondTimeContainer.swift
+//  Scoop
+//
+//  Created by Art Ostin on 23/03/2026.
+//
+
+import SwiftUI
+
+struct RespondTimeContainer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RespondTimeContainer()
+}
