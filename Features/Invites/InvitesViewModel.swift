8441d05da89f6cf94e64a98ecff9cf16b0d990f7
Arthur-Ostin <164878793+Arthur-Ostin@users.noreply.github.com>
2026-05-02 19:05:10 +0100

    Small Bug Fixes

--- Features/Invites/InvitesViewModel.swift
+++ Features/Invites/InvitesViewModel.swift
@@ -78,16 +78,14 @@ extension InvitesViewModel {
     
     private func updateInvitesLocally(eventId: String, isAccepted: Bool = false) {
         //1. IF - (a) declined, (b) accepted or (c) new invite sent with (i) new time or (ii) entirely new event - then remove invites in session
-        session.removeInvitedEventInSession(id: eventId)
+        session.removeEvent(id: eventId)
         
         //2. When responded also remove it from defaults, as event responses are stored
         defaults.deleteRespondDraft(eventId: eventId)
         
         //3. if accepted, update session Manager, to remove event from 'invites' and add it to events variable
         if isAccepted {
-            if let eventProfile = invites.first(where: { $0.id == eventId}) {
-                session.updateAcceptedEventInSession(eventProfile: eventProfile)
-            }
+            session.acceptInvite(eventId: eventId)
         }
     }
 }
