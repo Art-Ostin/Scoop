92520a2c41ee094ec8f97c12f415d0a8de940c21
Arthur-Ostin <164878793+Arthur-Ostin@users.noreply.github.com>
2026-07-28 17:48:01 +0400

    Begun simplifying and updating code to do with the respond invite section

---
+++ Features/Invites/Respond/Views/RespondInviteView.swift
@@ -0,0 +1,21 @@
+//
+//  RespondContainer.swift
+//  Scoop Test
+//
+//  Created by Art Ostin on 28/07/2026.
+//
+
+import SwiftUI
+
+struct RespondInviteView: View {
+    
+    var body: some View {
+        
+        
+        
+    }
+}
+
+#Preview {
+    RespondContainer()
+}
