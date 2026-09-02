98a59cae586e07222eb955fc484fda879fc71838
Arthur-Ostin <164878793+Arthur-Ostin@users.noreply.github.com>
2026-09-02 16:51:37 -0400

    Small

---
+++ Features/EventPopup/RespondToInvite/RespondToInviteContainer.swift
@@ -0,0 +1,18 @@
+//
+//  RespondToInviteContainer.swift
+//  Scoop Test
+//
+//  Created by Art Ostin on 02/09/2026.
+//
+
+import SwiftUI
+
+struct RespondToInviteContainer: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RespondToInviteContainer()
+}
